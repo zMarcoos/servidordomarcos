aa790d76921b1f0798c4131082aa3b6f35efc5bc
zMarcoos <59182008+zMarcoos@users.noreply.github.com>
2020-06-21 07:26:49 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  const ping = new Date();
+  ping.setHours(ping.getHours() - 3);
+  console.log(`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+const Discord = require('discord.js');
+const client = new Discord.Client({disableEveryone: true});
+require("./functions")(client);
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+
+module.exports = {
+    client: client
+};
